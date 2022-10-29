69a54646ea0d65bf29ae0d297c78695ee26b39bd
JaggedHorzion <rickrtimmers@gmail.com>
2022-10-29 14:15:15 +1100

    Removed double function

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const charactersOne = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-const charactersTwo = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 let passwordLength = 12
 
-function getRandomCharacterOne() {
-    let randomCharOne = Math.floor(Math.random() * charactersOne.length)
-    return charactersOne[randomCharOne]
-}
-
-function getRandomCharacterTwo() {
-    let randomCharTwo = Math.floor(Math.random() * charactersTwo.length)
-    return charactersTwo[randomCharTwo]
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
 }
 
 function generateRandomPasswordOne() {
     let randomPasswordOne = ""
     for (let i = 0; i < passwordLength; i++) {
-        randomPasswordOne += getRandomCharacterOne()         
+        randomPasswordOne += getRandomCharacter()         
     }
     return randomPasswordOne
 }
@@ -26,7 +19,7 @@ function generateRandomPasswordOne() {
 function generateRandomPasswordTwo() {
     let randomPasswordTwo = ""
     for (let i = 0; i < passwordLength; i++) {
-        randomPasswordTwo += getRandomCharacterTwo()         
+        randomPasswordTwo += getRandomCharacter()         
     }
     return randomPasswordTwo
 }
